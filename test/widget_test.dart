476f2b2bc5400d63095e8fb7578c38639a101e28
“Staxupgit config --global user.email “nuhshekhey@gmail.com <nuhshekhey@gmail.com>
2025-02-09 21:20:37 -0500

    complete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,25 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:counter_toggle_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments when button is pressed',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(CounterToggleApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Counter: 0'), findsOneWidget);
+    expect(find.text('Counter: 1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'Increment' button and trigger a frame.
+    await tester.tap(find.text('Increment'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Counter: 0'), findsNothing);
+    expect(find.text('Counter: 1'), findsOneWidget);
   });
 }
